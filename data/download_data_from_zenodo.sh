42725814d9c8322eabe353896014c4d0e088fc01
Simona Miller <simona.miller@ldas-pcdev5.ligo-la.caltech.edu>
2025-05-20 17:17:40 -0500

    script to download data release from zenodo

--- data/download_data_from_zenodo.sh
+++ data/download_data_from_zenodo.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Download and unzip 
-## [TO DO]
+curl https://zenodo.org/record/15474960/files/heavy_precessing_bbh_time_domain_data.zip --output "heavy_precessing_bbh_time_domain_data.zip"
+unzip heavy_precessing_bbh_time_domain_data.zip
+
+# Remove zip and Mac OSX files
+rm heavy_precessing_bbh_time_domain_data.zip
